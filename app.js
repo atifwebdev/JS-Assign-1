57f4e4c2a173f69a1f6137a11e057f76835d1c62
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-06-09 19:34:30 +0500

    add print table in console

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ const message = fullName + " is " + age + " years old. You can contact him/her o
 alert(message);
 
 document.getElementById("p-msg").innerText = message;
+
+
+
+
+// 2 - Ask the user to enter a number, then print table of the provided number in a console window.
+
+var finalOutput = "";
+var numGiven = +prompt("Enter any number to generate a table.");
+for( var i = 1; i <= 10; i++){
+    finalOutput += numGiven + " x " + i + " = " + numGiven*i + "\n";    //if print in DOM use <br>
+}
+console.log(finalOutput);
+// document.getElementById("p-table").innerHTML = finalOutput;
